5e3df7442116a553ad2f6e1ee5a692442b056f7a
camendoza94 <Arualr04>
2017-10-24 14:38:28 -0500

    Added ontologies and reading from CSV file.

--- src/main/java/com/camendoza94/semanticinterface/SemanticController.java
+++ src/main/java/com/camendoza94/semanticinterface/SemanticController.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.RDFNode;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("/interface")
@@ -31,14 +35,13 @@ class SemanticController {
         JsonParser parser = new JsonParser();
         JsonObject body = parser.parse(observation.getPayload()).getAsJsonObject();
         if (cache.get(deviceID) == null) {
-            List<AbstractMap.SimpleEntry<RDFNode, RDFNode>> matches = queryMatching();
+            HashMap<String, String> matches = getMatchingFromCSV();
             System.out.println("Device ID: " + deviceID);
-            String label = getMatchLabel(matches.get(0).getKey().toString(), deviceID);
+            String label = getDeviceValue(deviceID);
             if (StringUtils.isEmpty(label)) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Device not found");
             }
-            System.out.println("Label: " + label);
-            String[] service = getServicePostAPI(label, matches.get(0).getValue().toString());
+            String[] service = getServicePostAPI(matches.get(label));
             System.out.println("Service: " + Arrays.toString(service));
             if (service[0] == null || service[1] == null) {
                 return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Service not found");
@@ -73,20 +76,19 @@ class SemanticController {
         return template.postForEntity(URL, entity, String.class);
     }
 
-    private static String[] getServicePostAPI(String label, String matched) {
+    private static String[] getServicePostAPI(String matched) {
         String[] info = new String[4];
         String qs = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                 "PREFIX : <http://www.semanticweb.org/ca.mendoza968/ontologies/services#>\n" +
                 "SELECT ?URI ?methodValue (group_concat(?bodyLabel) as ?bodyLabels) (group_concat(?dataTypeLabel) as ?dataTypes)\n" +
                 "WHERE {\n" +
-                " ?service a :Service; (:|!:)* ?property .\n" +
-                " ?property a <" + matched + ">; rdfs:label '" + label + "' .\n" +
+                " ?service a :Service; :aboutProperty ?property .\n" +
                 " ?service :hasAPIURL ?URL; :hasMethod ?method .\n" +
                 " ?URL :hasStringValue ?URI .\n" +
-                " ?method a :Create; :hasStringValue ?methodValue .\n" +
-                " ?method :hasBodyField ?bodyField .\n" +
+                " ?method a :Create; :hasStringValue ?methodValue; :hasBodyField ?bodyField .\n" +
                 " ?bodyField rdfs:label ?bodyLabel; :hasDataType ?dataType .\n" +
                 " ?dataType rdfs:label ?dataTypeLabel .\n" +
+                "FILTER (?property = <" + matched + ">)\n" +
                 "}\n" +
                 "GROUP BY ?URI ?methodValue";
         QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/virtual/query", QueryFactory.create(qs));
@@ -104,14 +106,43 @@ class SemanticController {
         return info;
     }
 
-    private static String getMatchLabel(String matched, String deviceId) {
-        System.out.println("Matched Class: " + matched);
+//    private static String[] getServicePostAPI(String label, String matched) {
+//        String[] info = new String[4];
+//        String qs = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+//                "PREFIX : <http://www.semanticweb.org/ca.mendoza968/ontologies/services#>\n" +
+//                "SELECT ?URI ?methodValue (group_concat(?bodyLabel) as ?bodyLabels) (group_concat(?dataTypeLabel) as ?dataTypes)\n" +
+//                "WHERE {\n" +
+//                " ?service a :Service; (:|!:)* ?property .\n" +
+//                " ?property a <" + matched + ">; rdfs:label '" + label + "' .\n" +
+//                " ?service :hasAPIURL ?URL; :hasMethod ?method .\n" +
+//                " ?URL :hasStringValue ?URI .\n" +
+//                " ?method a :Create; :hasStringValue ?methodValue; :hasBodyField ?bodyField .\n" +
+//                " ?bodyField rdfs:label ?bodyLabel; :hasDataType ?dataType .\n" +
+//                " ?dataType rdfs:label ?dataTypeLabel .\n" +
+//                "}\n" +
+//                "GROUP BY ?URI ?methodValue";
+//        QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/virtual/query", QueryFactory.create(qs));
+//
+//        //TODO Body fields must be optional
+//        ResultSet results = exec.execSelect();
+//
+//        if (results.hasNext()) {
+//            QuerySolution next = results.next();
+//            info[0] = next.get("URI").toString();
+//            info[1] = next.get("methodValue").toString();
+//            info[2] = next.get("bodyLabels").toString();
+//            info[3] = next.get("dataTypes").toString();
+//        }
+//        return info;
+//    }
+
+    private static String getDeviceValue(String deviceId) {
         String qs = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
                 "PREFIX : <http://www.semanticweb.org/ca.mendoza968/ontologies/iotdevices#>\n" +
-                "SELECT ?label\n" +
+                "SELECT ?property\n" +
                 "WHERE {\n" +
-                " ?device a :Sensor; :hasDeviceId '" + deviceId + "'; (:|!:)* ?u .\n" +
-                " ?u a <" + matched + ">; rdfs:label ?label\n" +
+                " ?device a :Sensor; :hasDeviceId '" + deviceId + "'; (:|!:)* ?property .\n" +
+                " ?property a :Property .\n" +
                 "}";
         QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/fisica/query", QueryFactory.create(qs));
 
@@ -119,28 +150,65 @@ class SemanticController {
 
         if (results.hasNext()) {
             QuerySolution next = results.next();
-            return next.get("label").toString();
+            return next.get("property").toString();
         }
         return "";
     }
 
-    private static List<AbstractMap.SimpleEntry<RDFNode, RDFNode>> queryMatching() {
-        List<AbstractMap.SimpleEntry<RDFNode, RDFNode>> matches = new ArrayList<>();
-        String qs = "PREFIX align: <http://knowledgeweb.semanticweb.org/heterogeneity/alignment#>\n" +
-                "SELECT ?entity1 ?entity2\n" +
-                "WHERE {\n" +
-                "  ?subject align:entity1 ?entity1 .\n" +
-                "  ?subject align:entity2 ?entity2\n" +
-                "}";
-        QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/matching/query", QueryFactory.create(qs));
+//    private static String getMatchLabel(String matched, String deviceId) {
+//        System.out.println("Matched Class: " + matched);
+//        String qs = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\n" +
+//                "PREFIX : <http://www.semanticweb.org/ca.mendoza968/ontologies/iotdevices#>\n" +
+//                "SELECT ?label\n" +
+//                "WHERE {\n" +
+//                " ?device a :Sensor; :hasDeviceId '" + deviceId + "'; (:|!:)* ?u .\n" +
+//                " ?u a <" + matched + ">; rdfs:label ?label\n" +
+//                "}";
+//        QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/fisica/query", QueryFactory.create(qs));
+//
+//        ResultSet results = exec.execSelect();
+//
+//        if (results.hasNext()) {
+//            QuerySolution next = results.next();
+//            return next.get("label").toString();
+//        }
+//        return "";
+//    }
 
-        ResultSet results = exec.execSelect();
-
-        while (results.hasNext()) {
-            QuerySolution next = results.next();
-            matches.add(new AbstractMap.SimpleEntry<>(next.get("entity2"), next.get("entity1")));
+    private static HashMap<String,String> getMatchingFromCSV() {
+        HashMap<String,String> matches = new HashMap<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("src/data/link.csv"))) {
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                String iotClass = line.split(",")[1].trim();
+                String serviceClass = line.split(",")[2].trim();
+                matches.put(iotClass, serviceClass);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
         return matches;
     }
+
+//    private static List<AbstractMap.SimpleEntry<RDFNode, RDFNode>> queryMatching() {
+//        List<AbstractMap.SimpleEntry<RDFNode, RDFNode>> matches = new ArrayList<>();
+//        String qs = "PREFIX align: <http://knowledgeweb.semanticweb.org/heterogeneity/alignment#>\n" +
+//                "SELECT ?entity1 ?entity2\n" +
+//                "WHERE {\n" +
+//                "  ?subject align:entity1 ?entity1 .\n" +
+//                "  ?subject align:entity2 ?entity2\n" +
+//                "}";
+//        QueryExecution exec = QueryExecutionFactory.sparqlService("http://localhost:3030/matching/query", QueryFactory.create(qs));
+//
+//        ResultSet results = exec.execSelect();
+//
+//        while (results.hasNext()) {
+//            QuerySolution next = results.next();
+//            matches.add(new AbstractMap.SimpleEntry<>(next.get("entity2"), next.get("entity1")));
+//        }
+//
+//        return matches;
+//    }
 }
